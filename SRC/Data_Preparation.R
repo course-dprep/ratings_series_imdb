ddcea9f63217593060fb1ec05fa19c4c76f91eee
Julian Peters <j.a.m.peters@outlook.com>
2024-09-13 15:34:46 +0200

    Jibbe and Me cleaned the data

--- SRC/Data_Preparation.R
+++ SRC/Data_Preparation.R
@@ -1,4 +1,31 @@
 #Third file in the makefile
 #This file will clean the extracted information, hence it can be used in the analysis
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(here)
 
-print('file 3 run')
+# Creating the directory for output files
+output_dir <- here("gen", "data_preparation", "output")
+# Check whether the directory already exists
+if (!dir.exists(output_dir)) {
+  dir.create(output_dir, recursive = TRUE) 
+}
+
+# Combine datasets into one
+raw_data <- Episode_Data %>%
+  left_join(Genre_Data, by = "tconst") %>%
+  left_join(Ratings_Data, by = "tconst")
+
+# Filter out unused variables
+selected_columns = raw_data %>% select(-titleType, -primaryTitle, -originalTitle)
+
+# Filter out episodes with NA or less than 206 (the mean) votes
+filtered_data = selected_columns %>% filter(numVotes >=206)
+
+# Change column names
+colnames(filtered_data) = c( "episode_id", "show_id", "season_number", "episode_number", "is_for_adult", "start_year", "end_year", "episode_minutes", "genres", "rating", "n_votes")
+
+write.csv(filtered_data, here("gen", "data_preparation", "output", "cleaned_data.csv"), row.names = FALSE)
+
+print('file 3 run')
